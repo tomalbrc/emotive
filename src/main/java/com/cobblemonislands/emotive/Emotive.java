ed422ff081e3037c76f3d8f78ddfed67141f729d
tomalbrc <tomalbrc@me.com>
2025-10-17 06:04:05 +0200

    initial commit

---
+++ src/main/java/com/cobblemonislands/emotive/Emotive.java
@@ -0,0 +1,10 @@
+package com.cobblemonislands.emotive;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Emotive implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
